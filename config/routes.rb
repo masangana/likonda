122bc89014c9b23f3f195e50ab940c1b265872e9
masangana <alexmasangana@gmail.com>
2022-10-11 00:09:02 +0300

    Add splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root "splash#index"
+  get 'groups/index'
+  get 'groups/show'
+  get 'groups/new'
+  get 'groups/create'
+  get 'budgets/index'
+  get 'budgets/show'
+  get 'budgets/new'
+  get 'budgets/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
