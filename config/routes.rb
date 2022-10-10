87d60e1053579af5f3751cc8c9af2a7274cd2c72
masangana <alexmasangana@gmail.com>
2022-10-11 00:47:45 +0300

    Login et redirection reussi

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root "splash#index"
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/create'
-  get 'budgets/index'
-  get 'budgets/show'
-  get 'budgets/new'
-  get 'budgets/create'
-  devise_for :users
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+  resources :users
+  resources :groups do
+    resources :budgets
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
