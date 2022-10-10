649411f243675a02f85aa0aaf9a6803aadb53662
masangana <alexmasangana@gmail.com>
2022-10-11 01:42:55 +0300

    Add ability

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,13 +1,31 @@
 class BudgetsController < ApplicationController
   def index
+    @budgets = Budget.all.includes([:user]).where(user_id: current_user.id).order(:name)
   end
 
   def show
+    @budget = Budget.find(params[:id])
   end
 
   def new
+    @budget = Budget.new
   end
 
   def create
+    @user = current_user
+    @budget = @user.budgets.new(budget_params)
+    if @budget.save
+      @group_budget = @budget.group_budgets.create(group_id: params[:group_id])
+      flash[:success] = "Budget created!"
+      redirect_to budgets_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def budget_params
+    params.require(:budget).permit(:name, :amount)
   end
 end
