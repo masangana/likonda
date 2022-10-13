b193374762996c941f402cfd22cf9aa35ff3ccbf
masangana <alexmasangana@gmail.com>
2022-10-13 11:05:38 +0300

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
+  resources :users
   resources :groups do
     resources :budgets
   end
